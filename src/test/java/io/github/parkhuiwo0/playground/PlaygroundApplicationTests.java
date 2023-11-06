5aa0afad1933a86cb3a88c3bd5ca3975cd8216cd
parkhuiwo0 <huiwo0@naver.com>
2023-11-07 00:39:27 +0900

    init

---
+++ src/test/java/io/github/parkhuiwo0/playground/PlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.parkhuiwo0.playground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
